7de22ee8f2423d67032810885302829193e0e283
skraus5628 <s_read@aol.com>
2020-03-14 11:58:18 -0500

    setting up components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 
---
+++ client/src/components/login.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Login(props){
+    const username = useFormInput('');
+    const password = useFormInput('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleLogin = () =>{
+        props.history.push('/dashboard')
+    }
+
+    return(
+        <div>
+            Login <br /><br />
+            <div>
+                Username <br/>
+                <input type="text" {...username} autoComplete="new-password" />
+            </div>
+            <div style={{ marginTop: 10}}>
+                Password <br/>
+                <input type="password" {...password} autoComplete="new-password" />
+            </div>
+            {error && <><small style={{color: 'red'}}>{error}</small><br /></>}<br />
+            <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
+        </div>
+    )
+}
+
+const useFormInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+}
